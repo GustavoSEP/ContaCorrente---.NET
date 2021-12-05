85b7192fefede5029ab6d8eba8bdc971707b3cca
Gustavo Moreira <gustamoreira26@gmail.com>
2021-12-05 15:35:29 -0300

    Criação de conta

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 1823;
+            contaDaGabriela.numero = 28098;
+            contaDaGabriela.saldo = 1680.50;
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero: " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: " + contaDaGabriela.saldo);
+        
+
+            Console.ReadLine();
+        }
+    }
+}
